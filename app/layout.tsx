cc8298744bfe9cb3928eaec902afe9c00b04ffce
Pedro Rolfsen Ittner <pedro@MacBook-Air-de-Pedro.local>
2026-04-20 23:05:21 -0300

    fix: Forçada atualização de favicon e manifesto PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   title: "Hub Financeiro Bot",
   description: "Gerencie suas finanças com o poder da voz e Inteligência Artificial.",
   manifest: "/manifest.json",
-  themeColor: "#3182ce",
+  themeColor: "#0085E6",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
   appleWebApp: {
      capable: true,
